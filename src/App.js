1429519c9a5e90ad995f7712616196d79166b221
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-11-08 13:41:42 -0600

    update search form

--- src/App.js
+++ src/App.js
@@ -2,31 +2,35 @@ import React, { Component } from "react";
 import EmployeeCard from "./components/EmployeeCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import friends from "./friends.json";
+import worker from "./friends.json";
+import Form from "./components/Form";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    worker
   };
 
 
   render() {
     return (
-      <Wrapper>
-        <Title>Employee Directory</Title>
-        {this.state.friends.map(employee => (
-          <EmployeeCard
-            
-            id={employee.id}
-            key={employee.id}
-            name={employee.name}
-            image={employee.image}
-            occupation={employee.occupation}
-            location={employee.location}
-          />
-        ))}
-      </Wrapper>
+      <div className="container">
+        <Wrapper>
+          <Form />
+          <Title>Employee Directory</Title>
+
+          {this.state.worker.map(employee => (
+            <EmployeeCard
+              id={employee.id}
+              key={employee.id}
+              name={employee.name}
+              image={employee.image}
+              occupation={employee.occupation}
+              location={employee.location}
+            />
+          ))}
+        </Wrapper>
+        </div>  
     );
   }
 }
