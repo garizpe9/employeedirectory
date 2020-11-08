740e13947f4fa9e0c754738e84c66cd04f909160
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-11-08 12:58:39 -0600

    updating to include 'employees'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Jumbotron from "./components/Jumbotron";
-import Card from "./components/Card";
+import React, { Component } from "react";
+import EmployeeCard from "./components/EmployeeCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
 
-function App() {
-  return (
-    <div className="container">
-      <Navbar />
-      <Jumbotron />
-      <Card />
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends
+  };
+
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Employee Directory</Title>
+        {this.state.friends.map(employee => (
+          <EmployeeCard
+            
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            image={employee.image}
+            occupation={employee.occupation}
+            location={employee.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
