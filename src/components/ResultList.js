f6b88e59cf117baba5cb4c50f59af3975591026f
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-11-08 18:39:39 -0600

    updated employee pulls

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -4,8 +4,13 @@ function ResultList(props) {
   return (
     <ul className="list-group">
       {props.employee.map(result => (
-        <li className="list-group-item" key={result.cell}>
-          {/* <img alt={result.title} className="img-fluid" src={result.images.original.url} /> */}
+        <li className="list-group-item" key={result.login.uuid}>
+          {<h1>{result.name.first} {result.name.last}</h1>}
+          {<img alt={result.login.uuid} className="img-fluid" src={result.picture.large} />}<br></br>
+          {result.cell} <br></br>
+          {result.email} <br></br>
+          {result.location.city}, {result.location.state}
+          
         </li>
       ))}
     </ul>
